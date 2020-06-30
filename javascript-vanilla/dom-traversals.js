41748fa1ec29ad89080e0bda35150c3bcac43093
Chavez, Juan A <JACHAVEZ@pier1.com>
2020-06-29 23:29:11 -0500

    add children js

--- javascript-vanilla/dom-traversals.js
+++ javascript-vanilla/dom-traversals.js
@@ -1,6 +1,33 @@
-const component = document.querySelector('.component');
-const componentTitle = document.querySelector('.component__title');
+// const component = document.querySelector('.component');
+// const componentTitle = document.querySelector('.component__title');
+
+// const lionEmoji = document.querySelector('.lion')
+// const watchEmoji = document.querySelector('.watch')
+// const coffeeEmoji = document.querySelector('.coffee')
+// const peaceEmoji = document.querySelector('.peace')
+
+// console.log(component)
+// console.log(componentTitle)
+// console.log(lionEmoji)
+// console.log(watchEmoji)
+// console.log(coffeeEmoji)
+// console.log(peaceEmoji)
+
+const list = document.querySelector('.list');
+
+const listItems = list.children;
 
 
-console.log(component)
-console.log(componentTitle)
+console.log(listItems);
+
+
+const array = Array.from(HTMLCollection);
+array.forEach(el => {
+// do something
+});
+
+
+
+
+
+
