e028113e2cfe164e17f262e6455b255bb6317125
juanchavez <juanchavez11@yahoo.com>
2020-06-30 23:19:17 -0500

    dom traverse content aded

--- javascript-vanilla/dom-traversals.js
+++ javascript-vanilla/dom-traversals.js
@@ -1,6 +1,12 @@
-const component = document.querySelector('.component');
-const componentTitle = document.querySelector('.component__title');
+// const component = document.querySelector('.component');
+// const componentTitle = document.querySelector('.component__title');
 
 
-console.log(component)
-console.log(componentTitle)
+// console.log(component)
+// console.log(componentTitle)
+
+
+
+const list = document.querySelector('.list')
+
+list
