9935284aeac94bbf43b5dd65ec6d8d350b004f38
Chavez, Juan A <JACHAVEZ@pier1.com>
2020-07-01 00:28:51 -0500

    traverse samples added

--- javascript-vanilla/dom-traversals.js
+++ javascript-vanilla/dom-traversals.js
@@ -13,21 +13,81 @@
 // console.log(coffeeEmoji)
 // console.log(peaceEmoji)
 
-const list = document.querySelector('.list');
+// const list = document.querySelector('.list');
+// const listItems = list.children;
 
-const listItems = list.children;
-
-
-console.log(listItems);
+// console.log(listItems);
 
 
-const array = Array.from(HTMLCollection);
-array.forEach(el => {
-// do something
-});
+// const array = Array.from(HTMLCollection);
+// array.forEach(el => {
+// // do something
+// });
 
 
 
+// =============================================
+    // Selecting a specific child 
+// =============================================
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++ firstElementChild +++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// you can also select the first element with *** firstElementChild ***
+// const list = document.querySelectorAll('li');
+// const firstItem = list[0];
+//             // ==== OR ==== 
+// const list = document.querySelectorAll('.list');
+// const firstItem = list.firstElementChild
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// you can select the first element with children like this: const firstItem = listItems[0];
+
+// const listItems = document.querySelectorAll('.list');
+
+// const firstItem = listItems[0];
+// const secondItem = listItems[1];
+// const thirdItem = listItems[2];
+// const fourthItem = listItems[3];
+// const fifthItem = listItems[4];
+
+
+
+// console.log(firstItem);
+// console.log(secondItem);
+// console.log(thirdItem);
+// console.log(fourthItem);
+// console.log(fifthItem);
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++ Traversing upwards +++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// There are two ways to traverse upwards 
+// 1. parentElement
+// 2. closet
+
+
+  // <<<<<<<<<<<<<<<<<<<< parentElement >>>>>>>>>>>>>>>>
+  // const firstListItem = document.querySelector('li');
+  // const list = firstListItem.parentElement
+  // console.log(list) 
+
+
+
+  // <<<<<<<<<<<<<<<<<<<< closest >>>>>>>>>>>>>>>>
+  // To find an element that can be multiple levels above the current element 
+  // const closestAncestor = Element.closest(selector)
+
+const firstLink = document.querySelector('a')
+
+const list = firstLink.closest('.list') 
+console.log(list)
 
 
 
