95d04fa174187ad49c352c7dd3449993eef16765
juanchavez <juanchavez11@yahoo.com>
2020-08-08 23:39:37 -0500

    comment code

--- javascript-vanilla/introduction-to-ajax.js
+++ javascript-vanilla/introduction-to-ajax.js
@@ -3,20 +3,53 @@
 // request.open(method, link)
 // send the request out 
 
-const request = new XMLHttpRequest()
-request.addEventListener('load', e => {
-    const repos = JSON.parse(e.target.response)
-    console.log(repos)
-})
-request.open('get', 'https://api.github.com/users/Gitpersonal/repos')
-request.send();
+// const request = new XMLHttpRequest()
+// request.addEventListener('load', e => {
+//     const repos = JSON.parse(e.target.response)
+//     const data = repos.map(repo => {
+//         return {
+//             films: repo['https://swapi.dev/api/films/'],
+//             people: repo['https://swapi.dev/api/people/'],
+//             planets:repo['https://swapi.dev/api/planets/']
+//         }
+//     })
+//     const ol = document.createElement('ol')
+
+//     ol.innerHTML = data.map(repo => {
+//     return `<li>
+//                  <a href="${repo.url}">${repo.name} (${repo.stars} stars)</a>
+//            </li>`
+//     }).join('')
+
+//     document.body.appendChild(ol)
+// })
+// request.open('get', 'https://api.github.com/users/zellwk/repos')
+// request.send();
 
 
 
-const data = repos.map(repo => {
-    return {
-        name: repo.name,
-        url: repo['html_url'],
-        stars:repo['stargazers_count']
-    }
-})
+
+
+
+
+
+// const request = new XMLHttpRequest()
+// request.addEventListener('load', ev => {
+//   const repos = JSON.parse(ev.target.response)
+//   const htmlString = repos.map(repo => {
+//     return {
+//       name: repo.name,
+//       url: repo['html_url'],
+//       stars: repo['stargazers_count']
+//     }
+//   })
+//     .filter(repo => repo.stars > 50)
+//     .map(repo => `<li>${repo.name}: ${repo.stars} stars</li>`)
+//     .join('')
+
+//   const ol = document.createElement('ol')
+//   ol.innerHTML = htmlString
+//   document.body.appendChild(ol)
+// })
+// request.open('get', 'https://api.github.com/users/zellwk/repos')
+// request.send()
